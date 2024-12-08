8d06a6079489293ec4a2ae2eb982cc0bbec90718
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2024-12-07 18:26:03 -0600

    uso de directivas

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <button @click="decremento">-</button>
+  <span :class="miColor">{{ contador }}</span>
+  <button @click="incremento">+</button>
+  <div v-if="contador<0">Escribe una cantidad mayor a 0</div>
+  <div v-else-if="contador>=5">Escribe una cantidad menor a 5</div>
+  <div v-show="comprobacion">No esta permitido el 0</div>
+</template>
+
+<script setup>
+import {ref, computed } from "vue";
+const miColor =ref("azul")
+const contador =ref (0);
+const decremento =()=>{
+  contador.value--;
+  if (contador.value<0) {
+    miColor.value="rojo"
+  } 
+}
+
+const incremento =()=>{
+  contador.value++;
+  if (contador.value > (5) ) {
+    miColor.value="verde"
+  }
+}
+
+const comprobacion=computed(()=>{
+  return contador.value===0
+})
+</script>
+
+
+<style scoped>
+span {
+ padding: 5px;
+}
+.rojo{
+  color: red;
+}
+
+.verde{
+  color: green;
+}
+</style>
