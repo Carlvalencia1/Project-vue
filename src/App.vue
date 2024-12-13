e8a9aee4017da33a4d8805414fdf888aa2cdb9b1
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2024-12-13 14:13:42 -0600

    Integración de rutas 'imagenes'

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,15 @@
   <div v-if="contador<0">Escribe una cantidad mayor a 0</div>
   <div v-else-if="contador>=5">Escribe una cantidad menor a 5</div>
   <div v-show="comprobacion">No esta permitido el 0</div>
+  <img :src="image" alt="">
 </template>
 
 <script setup>
 import {ref, computed } from "vue";
+import {valores} from "./datos.js"
+
+// Declaracion de variables
+const image ="valores.web"
 const miColor =ref("azul")
 const contador =ref (0);
 const decremento =()=>{
---
+++ src/datos.js
@@ -0,0 +1,3 @@
+export const valores=[
+{web:"https://es.pngtree.com/freebackground/abstract-geometric-background-with-circles-lines-and-dots_15309460.html"}
+]
